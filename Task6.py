a30a25316ed6558ba51a9b13c84d98fcf2d714f6
TanishaM8 <s36863@caseygrammar.vic.edu.au>
2025-09-18 02:35:11 +0000

    bears

--- Task6.py
+++ Task6.py
@@ -1,26 +1,13 @@
-# Task 6 Find bear
-# Write a program to work out whether a line of input text contains a
-# bear or not. Your program needs to find cases where the word
-# appears in full, with no breaks.
+import re
 
 def main():
-    #Write your code here
     inputMessage = input("Enter line: ")
-    
-    # Check if 'bear' is in the input message
-    if 'bear' in inputMessage:
+
+    # Use regex to match the whole word 'bear'
+    if re.search(r'\bbear\b', inputMessage):
         print("There's a bear in there.")
     else:
         print("No bears here.")
-
-    
-        
-    
-    # End of your code here
-
-
-
-
 
 if __name__ == '__main__':
     main()
