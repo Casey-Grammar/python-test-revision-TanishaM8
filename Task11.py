383003c1122b559c2dd9dd5c9ed8b46e37640864
TanishaM8 <s36863@caseygrammar.vic.edu.au>
2025-09-18 01:44:29 +0000

    did all tasks. renamed task1 and fixed sample

--- Task11.py
+++ Task11.py
@@ -3,22 +3,20 @@
 # and editing the code until all errors are fixed.
 
 def main():
-    #Fix the code starting here
-    drink = input("WHAT DRINK DO YOU WANT TO ORDER?)
+    # Fix the code starting here
+    drink = input("WHAT DRINK DO YOU WANT TO ORDER? ")
+
     if drink.islower():
-     print("I DIDN'T HEAR YOUR ORDER!"
-      elif drink.isupper():
-      many = input("HOW MANY? ")
-    coffee scoops = many * 4
-    print(f'{many} {drink}S COMING RIGHT UP!')
-    print(f'The barrister needs {coffee scoops} scoops of coffee in the coffee making machine')
-    
+        print("I DIDN'T HEAR YOUR ORDER!")
+    elif drink.isupper():
+        many = int(input("HOW MANY? "))
+        coffee_scoops = many * 4
+        print(f"{many} {drink}S COMING RIGHT UP!")
+        print(f"The barista needs {coffee_scoops} scoops of coffee in the coffee making machine")
+    else:
+        print("PLEASE TYPE YOUR ORDER IN ALL CAPS!")
 
 # End of code to be fixed
-
-
-
-
 
 if __name__ == '__main__':
     main()
--- Task3.py
+++ Task3.py
@@ -3,9 +3,9 @@
 # Write a program that asks the user
 def main():
     #Write your code for input here
-    
-
-
+    dogYears = int(input("Dog years: "))
+    humanYears = dogYears * 7
+    print(f"Human years = {humanYears}")
     # End of your code for input here
 
 if __name__ == '__main__':
