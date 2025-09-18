7851425a34a5474f83f22a86ae76f3dd595a6678
TanishaM8 <s36863@caseygrammar.vic.edu.au>
2025-09-18 02:31:23 +0000

    CHANGED TASK 11

--- Task11.py
+++ Task11.py
@@ -3,18 +3,17 @@
 # and editing the code until all errors are fixed.
 
 def main():
-    # Fix the code starting here
-    drink = input("WHAT DRINK DO YOU WANT TO ORDER? ")
+    drink = input("WHAT DRINK DO YOU WANT TO ORDER? ").strip()
 
-    if drink.islower():
+    if not drink.isupper():
         print("I DIDN'T HEAR YOUR ORDER!")
-    elif drink.isupper():
-        many = int(input("HOW MANY? "))
-        coffee_scoops = many * 4
-        print(f"{many} {drink}S COMING RIGHT UP!")
-        print(f"The barista needs {coffee_scoops} scoops of coffee in the coffee making machine")
-    else:
-        print("PLEASE TYPE YOUR ORDER IN ALL CAPS!")
+        drink = input("WHAT DRINK DO YOU WANT TO ORDER? ").strip()
+
+    many = int(input("HOW MANY? "))
+    coffee_scoops = many * 4
+    print(f"{many} {drink}S COMING RIGHT UP!")
+    print(f"The barrister needs {coffee_scoops} scoops of coffee in the coffee making machine")
+
 
 # End of code to be fixed
 
